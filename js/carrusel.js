e47d59a6d17b1bd54b665bcb322a95fd9126e338
Jonathan Gonzalez <jona09_glz@outlook.com>
2025-11-22 20:45:03 -0600

    22-11-2025_20:44. Modificacion para carrusel ciclico

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,18 +2,30 @@ const carousel = document.querySelector(".carousel");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
+
+const SLIDE_WIDTH = 900; 
+
 let offset = 0;
 
+
+const maxOffset = -((carousel.children.length - 1) * SLIDE_WIDTH);
+
+
 nextBtn.addEventListener("click", () => {
-    if (offset > -((carousel.children.length - 1) * 470)) {
-        offset -= 470;
-        carousel.style.transform = `translateX(${offset}px)`;
+    if (offset <= maxOffset) {
+        offset = 0; 
+    } else {
+        offset -= SLIDE_WIDTH;
     }
+    carousel.style.transform = `translateX(${offset}px)`;
 });
 
+
 prevBtn.addEventListener("click", () => {
-    if (offset < 0) {
-        offset += 470;
-        carousel.style.transform = `translateX(${offset}px)`;
+    if (offset >= 0) {
+        offset = maxOffset; 
+    } else {
+        offset += SLIDE_WIDTH;
     }
+    carousel.style.transform = `translateX(${offset}px)`;
 });
